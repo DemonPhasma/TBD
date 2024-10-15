dc110009cafef3fb0b459ab1ed79636395a153e4
Бижова Андрей <andreybizhov@gmail.com>
2024-10-15 13:50:05 +0500

    папап

--- TBD/TBD.cpp
+++ TBD/TBD.cpp
@@ -1,38 +1,20 @@
 ﻿#include <iostream>
 
-void printArray(const int arr[], int size) {
-    std::cout << "Массив целых чисел: ";
-    for (int i = 0; i < size; ++i) {
-        std::cout << arr[i] << ' ';
+int multiply(int a, int b) {
+    if (b == 0) {
+        return 0;
     }
-    std::cout << std::endl;
-}
-
-void printArray(const char arr[], int size) {
-    std::cout << "Массив символов: ";
-    for (int i = 0; i < size; ++i) {
-        std::cout << arr[i] << ' ';
+    if (b < 0) {
+        return -multiply(a, -b);
     }
-    std::cout << std::endl;
-}
-
-void printArray(const double arr[], int size) {
-    std::cout << "Массив чисел с плавающей точкой: ";
-    for (int i = 0; i < size; ++i) {
-        std::cout << arr[i] << ' ';
-    }
-    std::cout << std::endl;
+    return a + multiply(a, b - 1);
 }
 
 int main() {
     setlocale(LC_ALL, "RU");
-    int intArray[5] = { 1, 2, 3, 4, 5};
-    char charArray[5] = { 'a', 'b', 'c', 'd', 'e' };
-    double doubleArray[5] = { 1.1, 2.2, 3.3, 4.4, 5.5 };
-
-    printArray(intArray, 5);
-    printArray(charArray, 5);
-    printArray(doubleArray, 5);
-
+    int num1 = 5;
+    int num2 = 3;
+    int result = multiply(num1, num2);
+    std::cout << "Произведение: " << num1 << " и " << num2 << " = " << result << std::endl;
     return 0;
 }
